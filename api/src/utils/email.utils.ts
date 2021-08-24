0f6de3206a54f7f17ca439091dcd035ad240f69f
Alex McConnell <amcconnell@hmbnet.com>
2021-08-24 13:43:44 -0400

    fix email and update env.example

--- api/src/utils/email.utils.ts
+++ api/src/utils/email.utils.ts
@@ -5,7 +5,6 @@ export const sendEmail = async (to: string, subject: string, text: string, html:
     let transportOptions = {
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD,
